6786dff586c62df0898fcc43e8fdad099b1efa57
Radar <radarlistener@gmail.com>
2008-03-20 14:08:26 +1030

    Removed vendor/rails frozen rails

--- vendor/rails/actionmailer/lib/action_mailer/vendor/tmail-1.2.2/tmail/index.rb
+++
@@ -1,8 +0,0 @@
-# This is here for Rolls. 
-# Rolls uses this instead of lib/tmail.rb.
-
-require 'tmail/version'
-require 'tmail/mail'
-require 'tmail/mailbox'
-require 'tmail/core_extensions'
-
--- vendor/rails/activesupport/lib/active_support/json/encoders/time.rb
+++
@@ -1,5 +0,0 @@
-class Time
-  def to_json(options = nil) #:nodoc:
-    %("#{strftime("%Y/%m/%d %H:%M:%S")} #{formatted_offset(false)}")
-  end
-end
