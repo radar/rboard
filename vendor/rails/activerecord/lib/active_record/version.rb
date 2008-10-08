5ef6ae64efdf33a8a3c8aeb2db84bcc333377e96
Ryan Bigg <radarlistener@gmail.com>
2008-10-08 15:42:02 +1030

    Remove edge rails because it appears to break stuff.

--- vendor/rails/activerecord/lib/active_record/version.rb
+++
@@ -1,9 +0,0 @@
-module ActiveRecord
-  module VERSION #:nodoc:
-    MAJOR = 2
-    MINOR = 2
-    TINY  = 0
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
--- vendor/rails/railties/lib/rails/version.rb
+++
@@ -1,9 +0,0 @@
-module Rails
-  module VERSION #:nodoc:
-    MAJOR = 2
-    MINOR = 2
-    TINY  = 0
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
