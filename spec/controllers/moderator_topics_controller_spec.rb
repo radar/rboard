062389b24d766521f41be5fb5b527a2bd737f2a9
Ryan Bigg <radarlistener@gmail.com>
2008-10-20 23:46:17 +1030

    Is that another test?!
    Wow, that's so hot AND DRY.

--- spec/controllers/moderator_topics_controller_spec.rb
+++ spec/controllers/moderator_topics_controller_spec.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-describe Moderator::TopicsController, "as moderator" do
+
+describe Moderator::TopicsController do
   
   fixtures :topics, :users, :forums, :moderations
   before do
@@ -11,73 +12,44 @@ describe Moderator::TopicsController, "as moderator" do
     @moderations = [@moderation]
   end
   
-  it "should not be able to lock any topic in the admin forum" do
-    Topic.should_receive(:find).and_return(@topic)
-    @topic.should_receive(:forum).and_return(@forum)
-    @forum.should_receive(:viewable?).and_return(false)
-    @topic.should_receive(:moderations).and_return(@moderations)
-    @moderations.should_receive(:for_user).and_return(@moderations)
-    @moderation.should_receive(:destroy).and_return(@moderation)
+  
+  describe Moderator::TopicsController, "as moderator" do
     
-    @topic.should_not_receive(:toggle!)
-    put 'toggle_lock', { :id => @admin_topic.id }
-    response.should redirect_to(moderator_moderations_path)
-  end
+    def find_topic_mock
+      Topic.should_receive(:find).and_return(@topic)
+      @topic.should_receive(:forum).and_return(@forum)
+      @forum.should_receive(:viewable?).and_return(false)
+      @topic.should_receive(:moderations).and_return(@moderations)
+      @moderations.should_receive(:for_user).and_return(@moderations)
+      @moderation.should_receive(:destroy).and_return(@moderation)
+    end
     
-    # it "should be able to unlock any topic in the admin forum" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      @topic.should_receive(:unlock!).and_return(@topic)
-    #      @topic.should_receive(:forum).and_return(@forum)
-    #      @forum.should_receive(:topics).and_return(@topics)
-    #      @topics.should_receive(:find).with(@admin_topic.id.to_s, :joins => :posts).and_return(@topic)
-    #      @topic.stub!(:has_key?).and_return(true)
-    #      put 'unlock', { :id => @admin_topic.id, :forum_id => @admin_forum.id }
-    #    end
-    #    
-    #  TODO: Move these to moderations controller spec testing.    
-    #    it "should be able to lock multiple topics" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      Topic.should_receive(:find).twice.and_return(@topic)
-    #      @topic.should_receive(:lock!).twice
-    #      post 'moderate', { :commit => "Lock", :moderated_topics => [1,2], :forum_id => @admin_forum.id } 
-    #      flash[:notice].should eql("All selected topics have been locked.")
-    #    end
-    #    
-    #    it "should be able to unlock multiple topics" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      Topic.should_receive(:find).twice.and_return(@topic)
-    #      @topic.should_receive(:unlock!).twice
-    #      post 'moderate', { :commit => "Unlock", :moderated_topics => [1,2], :forum_id => @admin_forum.id } 
-    #      flash[:notice].should eql("All selected topics have been unlocked.")
-    #    end
-    #    
-    #    it "should be able to destroy multiple topics" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      Topic.should_receive(:find).twice.and_return(@topic)
-    #      @topic.should_receive(:destroy).twice.and_return(@topic)
-    #      post 'moderate', { :commit => "Delete", :moderated_topics => [1,2], :forum_id => @admin_forum.id } 
-    #      flash[:notice].should eql("All selected topics have been deleted.")
-    #    end
-    #    
-    #    it "should be able to sticky multiple topics" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      Topic.should_receive(:find).twice.and_return(@topic)
-    #      @topic.should_receive(:sticky!).twice
-    #      post 'moderate', { :commit => "Sticky", :moderated_topics => [1,2], :forum_id => @admin_forum.id } 
-    #      flash[:notice].should eql("All selected topics have been stickied.")
-    #    end
-    #    
-    #    it "should be able to unsticky multiple topics" do
-    #      Forum.should_receive(:find).and_return(@forum)
-    #      @forum.should_receive(:viewable?).and_return(true)
-    #      Topic.should_receive(:find).twice.and_return(@topic)
-    #      @topic.should_receive(:unsticky!).twice
-    #      post 'moderate', { :commit => "Unsticky", :moderated_topics => [1,2], :forum_id => @admin_forum.id } 
-    #      flash[:notice].should eql("All selected topics have been unstickied.")
-    #    end
+    def not_allowed
+      flash[:notice].should eql("You are not allowed to access that topic.")
+      response.should redirect_to(moderator_moderations_path)
+    end
+    
+  
+    it "should not be able to toggle the locked status any topic in the admin forum" do
+      find_topic_mock
+      @topic.should_not_receive(:toggle!)
+      put 'toggle_lock', { :id => @admin_topic.id }
+      not_allowed
+    end
+  
+    it "should not be able to toggle the stickied status any topic in the admin forum" do
+      find_topic_mock
+      @topic.should_not_receive(:toggle!)
+      put 'toggle_sticky', { :id => @admin_topic.id }
+      not_allowed
+    end
+    
+    it "should not be able to delete a topic in the admin forum" do
+      find_topic_mock
+      @topic.should_not_receive(:destroy)
+      delete 'destroy', { :id => @admin_topic.id }
+      not_allowed
+    end
+    
   end
+end
