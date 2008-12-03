dba6b2554740825f07925d4b84579647258989b4
Ryan Bigg <radarlistener@gmail.com>
2008-12-03 14:16:30 +1030

    Ok, so current_page? doesn't work as well as it should.

--- app/helpers/namespaced_helper.rb
+++ app/helpers/namespaced_helper.rb
@@ -1,11 +1,13 @@
 module NamespacedHelper
   def selected(name)
-    'selected' if (current_page?(:controller => "admin/index") && name == "index") ||
-                  (current_page?(:controller => "admin/users") && name == "users" && params[:action] != "ban_ip") ||
-                  (current_page?(:controller => "admin/ranks") && name == "ranks") ||
-                  (current_page?(:controller => "admin/themes") && name == "themes") ||
-                  (current_page?(:controller => "admin/forums") && name == "forums") ||
-                  (current_page(:controller => "admin/users") && name == "ip_banning" && params[:action] == "ban_ip")
-                  (current_page(:controller => "moderator/moderations") && name == "moderations")
+    if (params[:controller] == "admin/index" && name == "index") ||
+       (params[:controller] == "admin/users" && name == "users" && params[:action] != "ban_ip") ||
+       (params[:controller] == "admin/ranks" && name == "ranks") ||
+       (params[:controller] == "admin/themes" && name == "themes") ||
+       (params[:controller] == "admin/forums" && name == "forums") ||
+       (params[:controller] == "admin/users" && name == "ip_banning" && params[:action] == "ban_ip")
+       (params[:controller] == "moderator/moderations" && name == "moderations")
+      'selected'
+    end
   end
 end
