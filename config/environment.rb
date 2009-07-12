2cfcdc7013fa48eb51fa64ed0fdec49e266af053
Ryan Bigg <radarlistener@gmail.com>
2009-07-12 16:11:15 +1000

    Inspired by all the people who try running rake db:migrate to set up the application (which causes db/schema.rb to be wiped clean!) and then try to run rake install later on. Thanks go to Dave Ekhaus for the chat earlier today.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
+require 'config/migration_hack'
 # Be sure to restart your web server when you modify this file.
 # Uncomment this to force production mode.
 # ENV['RAILS_ENV'] = 'development'
-
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
---
+++ config/migration_hack.rb
@@ -0,0 +1,16 @@
+# I love the smell of hax in the morning.
+if ARGV[0] == "db:migrate"
+  
+  lines = File.readlines("config/environment.rb")
+  lines.delete_if { |l| l == "require 'migration_hack'"}
+  f = File.open("config/environment.rb", "w+")
+  f.write(lines.join("\n"))
+  f.close
+  
+  FileUtils.rm(__FILE__)
+  raise "If you are REALLY REALLY SURE you want to run rake db:migrate consider this first:
+        If you have not yet run rake install (to install rBoard), running db:migrate will override db/schema.rb. This is a Bad Thing(tm).
+        This is your first, last and ONLY warning. Running rake db:migrate again will result in the expected results.
+        You have been warned!"
+
+end
