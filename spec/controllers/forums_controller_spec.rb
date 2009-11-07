e8e3de53fd752afea51d7e197b6e8c54dc9164c9
Ryan Bigg <radarlistener@gmail.com>
2009-11-07 21:28:39 +1000

    Removing controller specs that have been converted over into features.

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -20,14 +20,6 @@ describe ForumsController do
       login_as(:plebian)
     end
 
-    it "should be able to see a list of forums and categories" do
-      Category.should_receive(:without_parent).and_return(@categories)
-      Forum.should_receive(:without_category).and_return(@forums)
-      @forums.should_receive(:without_parent).and_return(@forums)
-      get 'index'
-      response.should render_template("index")
-    end
-
     it "should not be able to see anything inside a restricted category" do
       get 'index', :category_id => @admin_category.id
       flash[:notice].should eql(t(:category_permission_denied))
@@ -41,33 +33,4 @@ describe ForumsController do
     end
 
   end
-
-  describe "admin" do
-    before do
-      login_as(:administrator)
-    end
-
-    it "should be able to see the everybody forum" do
-      get 'show', :id => @everybody.id
-      response.should render_template("show")
-    end
-
-    it "should be able to see the admins only forum" do
-      get 'show', :id => @admins_only.id
-      response.should render_template("show")
-    end
-
-    it "should be able to see forums for the test category" do
-      find_forums
-      get 'index', :category_id => @test_category.id
-      response.should render_template("index")
-    end
-
-    it "should be able to see the forums for the admin category" do
-      find_forums
-    get 'index', :category_id => @admin_category.id
-      response.should render_template("index")
-    end
-  end
-
 end
