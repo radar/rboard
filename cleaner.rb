caa38c6fc73b294dfa263eced3cbd14d54054ed9
Ryan Bigg <radarlistener@gmail.com>
2009-11-07 18:59:23 +1000

    Update cleaner.

--- cleaner.rb
+++ cleaner.rb
@@ -1,4 +1,6 @@
 for file in Dir["app/**/*"]
-  lines = File.readlines(file).map { |line| line.gsub(/^\s+$/, "") }
-  File.open(file, "w+") { |f| f.write(lines.join("\n")) }
+  if File.file?(file)
+    lines = File.readlines(file).map { |line| line.gsub(/^\s+$/, "") }
+    File.open(file, "w+") { |f| f.write(lines.join) }
+  end
 end
