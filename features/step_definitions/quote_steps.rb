5e6fad413ded5f39055e51297ff175dcd217a32f
Ryan Bigg <radarlistener@gmail.com>
2010-02-06 11:55:42 +1100

    The quotee tag is now a strong, not b.

--- features/step_definitions/quote_steps.rb
+++ features/step_definitions/quote_steps.rb
@@ -1,5 +1,5 @@
 Then /^I should see a quote by "([^\"]*)" that says "([^\"]*)"$/ do |name, quote|
   quote_box = Nokogiri::HTML(response.body).css(".post .quote").first
-  quote_box.css("b").text.should eql("#{name} wrote:")
+  quote_box.css("strong").text.should eql("#{name} wrote:")
   quote_box.css("span").text.should eql(quote)
 end
