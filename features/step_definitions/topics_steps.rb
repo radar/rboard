ddf7a1315e5efd6df4bab9420965de2a0707aad0
Ryan Bigg <radarlistener@gmail.com>
2010-02-06 16:53:06 +1100

    Remove debug line.

--- features/step_definitions/topics_steps.rb
+++ features/step_definitions/topics_steps.rb
@@ -3,6 +3,5 @@ Then /^I should see (\d+) sticky topics?$/ do |num|
 end
 
 Then /^I should see (\d+) locked topics?$/ do |num|
-  p Topic.all.map(&:locked)
   Nokogiri::HTML(response.body).xpath("//img[@src]").select { |i| i['src'].match(/locked/) }.size.should eql(num.to_i)
 end
