bdf3780916e9c6a9804c2970b75bb3cc8f23db65
Chris Darroch <chris@chrisdarroch.com>
2010-06-11 19:55:57 +1000

    Base#named_scope is deprecated. Use Base#scope instead.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -19,7 +19,7 @@ class Topic < ActiveRecord::Base
   has_many :subscribers, :through => :subscriptions, :class_name => "User"
   has_many :users, :through => :posts
 
-  named_scope :sorted, :order => "posts.created_at DESC, sticky DESC", :include => [:last_post, :readers]
+  scope :sorted, :order => "posts.created_at DESC, sticky DESC", :include => [:last_post, :readers]
 
   #makes error_messages_for return the wrong number of errors.
   validates_associated :posts, :message => nil
