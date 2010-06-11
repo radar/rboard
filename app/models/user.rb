dd6fa9eda7cd7d81990c3d927d22b8e749332f90
Chris Darroch <chris@chrisdarroch.com>
2010-06-11 19:56:31 +1000

    Base#save(false) is deprecated. Use Base#save(:validate => false) instead.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,13 +32,13 @@ class User < ActiveRecord::Base
   def remember_me
     self.remember_token_expires_at = 2.weeks.from_now.utc
     self.remember_token            = encrypt("#{email}--#{remember_token_expires_at}")
-    save(false)
+    save(:validate => false)
   end
 
   def forget_me
     self.remember_token_expires_at = nil
     self.remember_token            = nil
-    save(false)
+    save(:validate => false)
   end
 
   protected
