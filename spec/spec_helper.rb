90f717d9c69f21144ec46529fc9d2a84f714d066
Chris Darroch <chris@chrisdarroch.com>
2010-08-01 15:37:33 +1000

    Use transactional fixtures for our specs.
    It seems the default in rSpec for running specs is no longer to use transactions...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,5 @@ Rspec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  # If you'd prefer not to run each of your examples within a transaction,
-  # uncomment the following line.
-  # config.use_transactional_examples = false
+  config.use_transactional_examples = true
 end
