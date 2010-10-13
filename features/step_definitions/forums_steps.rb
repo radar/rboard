c6bda20b7f75be83d33248b4e2718b7af2087e7c
Chris Darroch <chris@chrisdarroch.com>
2010-10-13 16:54:11 +1100

    Add an IP to each create statement when building the closed forum.

--- features/step_definitions/forums_steps.rb
+++ features/step_definitions/forums_steps.rb
@@ -3,10 +3,12 @@ Given /^there is an inactive forum$/ do
 end
 
 Given /^there is a closed forum$/ do
-  f = Forum.create!(:title => "Closed Forum", :description => "This is a closed forum", :open => false)
-  t = f.topics.build(:subject => "Closed Forum's Topic", :user => User.first)
-  t.posts.build(:text => "I live in the Closed Forum. All by myself.", :user => User.first)
-  t.save!
+  user = User.first
+  localhost = Ip.make(:localhost)
+  forum = Forum.create!(:title => "Closed Forum", :description => "This is a closed forum", :open => false)
+  topic = forum.topics.build(:subject => "Closed Forum's Topic", :user => user, :ip => localhost)
+  topic.posts.build(:text => "I live in the Closed Forum. All by myself.", :user => user, :ip => localhost)
+  topic.save!
 end
 
 Then /^I should be in the "([^\"]*)" forum$/ do |title|
